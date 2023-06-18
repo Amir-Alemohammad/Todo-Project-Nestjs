6149e9b95d708fa9d248707a0215214ae103e74f
Amir Alemohammad <amirho3inalemohammad@gmail.com>
2023-06-18 14:08:03 +0430

    add .env file for environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,18 @@ import { LoggerMiddleware } from './logger/logger.middleware';
 import { UsersModule } from './users/users.module';
 import { TodosModule } from './todos/todos.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: "postgres",
-      host:"localhost",
-      port: 5432,
-      username: "postgres",
-      password: "amir13848431",
-      database: "postgres",
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true, // for production mode this property is false
     }),
